731ee706d432e40803f78cb4a0dc3cbd637587e0
Malo BURNET <maburnet@e3r8p13.42.fr>
2024-07-23 19:21:45 +0200

    added scoring and playing field

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+
 // standard global variables
-var scene, camera, renderer, controls;
+var scene, camera, renderer, controls, loader;
 // var keyboard = new THREEx.KeyboardState();
 
 // custom global variables
-var cube, ball, paddle, paddleOutline, paddle2, paddleOutline2, keys;
+var cube, ball, paddle, paddleOutline, paddle2, paddleOutline2, keys, textMesh;
 const PADDLE_SPEED = 0.2;
 const BALL_SPEED = 0.1;
 const BALL_SIZE = 0.2;
 const MAX_HEIGHT = 4.5; // idk how to name this
-const MIN_HEIGHT = -MAX_HEIGHT;
+const MIN_HEIGHT = -4.5;
 var ballSpeed = {x: BALL_SPEED, y: BALL_SPEED};
+var leftPlayerScore = 0;
+var rightPlayerScore = 0;
 
+// TODO: I think i should load everything (all font, textures...) before initializing the rest
+// Load();
 Init();
 Loop();
 
+function createScoreText()
+{
+    if (textMesh) {
+        scene.remove(textMesh);
+        textMesh.geometry.dispose();
+        textMesh.material.dispose();
+    }
+    loader.load( 'fonts/roboto_condensed.json', function ( font ) {
+
+        const textGeometry = new TextGeometry( leftPlayerScore + "  :  " + rightPlayerScore, {
+            font: font,
+            size: 1,
+            // !!!! Use height and not depth
+            height: 0.1,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 5
+        });
+
+        const textMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+        textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        
+        // Compute the bounding box and center the text
+        // textGeometry.computeBoundingBox();
+        // const boundingBox = textGeometry.boundingBox;
+        // const size = new THREE.Vector3();
+        // boundingBox.getSize(size);
+        // scoreMesh.position.x = -size.x / 2;
+        // scoreMesh.position.y = -size.y / 2;
+        // scoreMesh.position.z = -size.z / 2;
+
+        textMesh.position.y += 6;
+        textMesh.position.z += 1.5;
+        textMesh.rotateX(45);
+        textMesh.position.x -= 1.5;
+
+        scene.add(textMesh);
+    } );
+}
 
 function Init()
 {
@@ -26,8 +75,8 @@ function Init()
     var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
     var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 2000;
     camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-    camera.position.set(0, -10, 10);
-    camera.lookAt(scene);
+    camera.position.set(0, -11, 13);
+    camera.lookAt(0, 0, 0);
     scene.add(camera);
 
     // RENDERER
@@ -76,35 +125,41 @@ function Init()
 
 
     // CUSTOM GEOMETRY
-    // const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const geometry = new THREE.SphereGeometry(4, 64, 32);
     const arenaFloorGeometry = new THREE.BoxGeometry(16, 9, 0.5);
     const arenaSmallSideGeometry = new THREE.BoxGeometry(0.5, 10, 0.5);
     const arenaLargeSideGeometry = new THREE.BoxGeometry(17, 0.5, 0.5);
     const paddleGeometry = new THREE.BoxGeometry(0.2, 2, 0.2);
     const ballGeometry = new THREE.SphereGeometry(BALL_SIZE, 64, 32);
     const points = [];
-    points.push( new THREE.Vector3( -8, MAX_HEIGHT, 0 ) );
-    points.push( new THREE.Vector3( 8, MAX_HEIGHT, 0 ) );
-    points.push( new THREE.Vector3( 8, MIN_HEIGHT, 0 ) );
-    points.push( new THREE.Vector3( -8, MIN_HEIGHT, 0 ) );
-    points.push( new THREE.Vector3( -8, MAX_HEIGHT, 0 ) );
+    points.push( new THREE.Vector3( 0, MAX_HEIGHT, -0.20 ) );
+    points.push( new THREE.Vector3( 0, MIN_HEIGHT, -0.20) );
 
     const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
 
     // TEXTURES
     const textureLoader = new THREE.TextureLoader();
-    // const cubeTexture = textureLoader.load( "./textures/cube_texture.png");
+    // const cubeMap = textureLoader.load( "./textures/honeycomb/Honeycomb_basecolor.jpg");
+    // const cubeAoMap = textureLoader.load( "./textures/honeycomb/Honeycomb_ambientOcclusion.jpg");
+    // const cubeRoughnessMap = textureLoader.load( "textures/honeycomb/Honeycomb_roughness.jpg");
+    // const cubeHeightMap = textureLoader.load( "textures/honeycomb/Honeycomb_height.png");
+    // const cubeNormalMap = textureLoader.load( "textures/honeycomb/Honeycomb_normal.jpg");
     const ballTexture = textureLoader.load("./textures/ball_texture.png")
 
     // MATERIAL
-    const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    const lineMaterial = new THREE.LineDashedMaterial( { color: 0x353535, linewidth: 1, scale: 1, dashSize: 0.5, gapSize: 0.5 } );
     const paddleMaterial = new THREE.MeshBasicMaterial({ color: 0x353535 });
     const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
     const blackMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
     const paddleOutlineMaterial = new THREE.MeshBasicMaterial({ color: 0xd1d1d1, side: THREE.BackSide });
-    // const cubeMaterial = new THREE.MeshStandardMaterial({map: cubeTexture}); 
+    // const cubeMaterial = new THREE.MeshStandardMaterial({map: cubeMap, aoMap: cubeAoMap, roughnessMap: cubeRoughnessMap, normalMap: cubeNormalMap}); 
     const ballMaterial = new THREE.MeshBasicMaterial({map: ballTexture});
 
+
+    // TEXT
+    loader = new FontLoader();
+
+    createScoreText();
 
     // MESH
     const arenaFloor = new THREE.Mesh(arenaFloorGeometry, blackMaterial);
@@ -133,11 +188,11 @@ function Init()
     scene.add(paddleOutline);
     scene.add(paddleOutline2);
     scene.add(paddle2);
-    // scene.add(line); // add back single line for middle of board
+    scene.add(line);
 
 
+    line.computeLineDistances();
     arenaFloor.position.z -= 0.5;
-
     arenaTopSide.position.y += 4.75;
     arenaBottomSide.position.y -= 4.75;
     arenaRightSide.position.x += 8.25;
@@ -203,8 +258,18 @@ function Update()
     ball.rotation.x += 2 * ballSpeed.x;
     ball.rotation.y += 2 * ballSpeed.y;
 
-    if (ball.position.x - BALL_SIZE < -8 || ball.position.x + BALL_SIZE > 8)
-        ballSpeed.x = -ballSpeed.x;
+    if (ball.position.x - BALL_SIZE < -8)
+    {
+        rightPlayerScore += 1;
+        createScoreText();
+        ballSpeed.x = -ballSpeed.x; //
+    }
+    if (ball.position.x + BALL_SIZE > 8)
+    {
+        leftPlayerScore += 1;
+        createScoreText();
+        ballSpeed.x = -ballSpeed.x; //
+    }
     if (ball.position.y - BALL_SIZE < MIN_HEIGHT || ball.position.y + BALL_SIZE > MAX_HEIGHT)
         ballSpeed.y = -ballSpeed.y;
 
